b49cb3b7ba698d17341775221e660a19fd799eec
ryanandrade78 <ryan.osolev@outlook.com>
2023-08-24 11:58:43 -0300

    updates

--- script.js
+++ script.js
@@ -4,3 +4,33 @@ const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordConfirmation = document.getElementById("password-confirmation");
 
+form.addEventListener("submit", (event)=>{
+    event.preventDefault();
+
+    checkInputUsername();
+})
+
+
+function checkInputUsername(){
+    const usernameValue = username.value;
+
+    if(usernameValue === ""){
+      errorInput(username, "Preencha um username!")
+    }else{
+        const form = username.parentElement;
+        formItem.classList = "form-content"
+    }
+
+
+    
+}
+
+function errorInput(input, message){
+    const formItem = input.parentElement;
+    const textMessage = formItem.querySelector("a")
+
+    textMessage.innerText = message;
+ 
+    formItem.className = "form-content error"
+
+}
