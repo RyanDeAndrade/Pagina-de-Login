7037f726dbc951c968df7adf712e1d3d33d43f2a
ryanandrade78 <ryan.osolev@outlook.com>
2023-08-24 11:00:39 -0300

    adcionando o js

---
+++ script.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirmation = document.getElementById("password-confirmation");
+
